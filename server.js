7cf5a2b2d8ed53dcddce97dbaafca580b6a5501e
Rakil Ahmed <imrakilahmed@gmail.com>
2020-07-11 00:32:57 -0400

    Morgan added, rotating-file-stream added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // REQUIRED DEPENDENCIES
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
 require('dotenv').config();
 
 // App + PORT + Database URL
@@ -8,9 +11,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const CONNECTION_URI = process.env.MONGODB_URI || process.env.DB_URI;
 
-// Routes
-const covidDataRoute = require('./routes/data-routes');
-app.use(covidDataRoute);
+// Middleware + Routes
+// DEV (TEST)
+app.use(
+  morgan('dev', {
+    skip: function (req, res) {
+      return res.statusCode < 400;
+    },
+  })
+);
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream('access.log', {
+  size: '10M', // rotate every 10 MegaBytes written
+  interval: '1d', // rotate daily
+  path: path.join('log'),
+});
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+
+const covidRoutes = require('./routes/data-routes');
+app.use(covidRoutes);
 
 // Connect to MongoDB -- Local Database
 mongoose.connect(
